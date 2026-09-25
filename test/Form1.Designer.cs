e0c7fd47c4b24e16ad083bf9539e092ff4930ccc
Alexander Sandberg <alexandersandbergnystart@gmail.com>
2026-09-26 00:17:26 +0200

    liten text änding

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -83,9 +83,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(190, 64);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(15, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.label3.Text = "hi";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // Form1
